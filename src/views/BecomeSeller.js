ea746fe695d159fe8b43e2600be048e982cb63ff
Waelbenmustapha <wbenmustapha@yahoo.fr>
2022-01-23 15:49:52 +0100

    Navbar changed upgradded new icons

--- src/views/BecomeSeller.js
+++ src/views/BecomeSeller.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 function BecomeSeller() {
     return (
-        <div style={{width:'1200px',border:'1px solid',margin:'0px auto',backgroundColor:'#ccc'}}>
-            <h1 style={{textAlign:'center'}}>Become A seller</h1>
-        </div>
+        <div></div>
     )
 }
 
